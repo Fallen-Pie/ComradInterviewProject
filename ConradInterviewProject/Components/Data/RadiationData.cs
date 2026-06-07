7703f1fc4711238c5b47b8e2a00023a4db8080ff
Blake Davis <brickanimator@gmail.com>
2026-06-07 17:09:05 +1200

    Fixed Incorrect Filepath (Again)

--- ConradInterviewProject/Components/Data/RadiationData.cs
+++ ConradInterviewProject/Components/Data/RadiationData.cs
@@ -7,6 +7,6 @@ public static class RadiationData
     static RadiationData()
     {
         DataImporter radiationData = new();
-        RadiationLocations = radiationData.ImportData("RadiationDose.csv");
+        RadiationLocations = radiationData.ImportData("Components/Data/RadiationDose.csv");
     }
 }
